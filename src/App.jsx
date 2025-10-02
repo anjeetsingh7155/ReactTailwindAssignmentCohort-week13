c3c14cbe9bdbfe0a02ba069b863567cced6acbfe
anjeetsingh7155 <anjeetsingh7155>
2025-10-02 12:23:20 +0530

    Created a new folder anwers in component and a new component 1-basic-Project.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,9 @@ function App() {
   const [disable, setDisable] = useState(false);
 
   return (
-    <div className="flex justify-center items-center bg-blue-800 h-screen text-white">
-      <div>
-        <div className="flex flex-col justify-center items-center font-mono text-3xl m-12 gap-25">
-          <div className="flex">
-            <img src="/src/assets/webinar.png" className="h-10 w-10" />
-            <h2 className="text-green-200">Webinar</h2>
-            <h2>.gg</h2>
-          </div>
-          <div>
-            <h2>Verify Your Age</h2>
-          </div>
-        </div>
-        <div>
-          <OtpBox />
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <h3 className="text-blue-100">
-            Please Confirm Your Birth Year. This Data will Not Be Sorted.
-          </h3>
-          <InputTab type="text" placeholder="Your Birth Year" />
-          <Button disable={disable}>Continue</Button>
-        </div>
-      </div>
-    </div>
+   <div>
+    
+   </div>
   );
 }
 
